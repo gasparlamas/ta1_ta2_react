7db23e88bd162fe1a75dd1374ec8b597090a438e
gasparlamas <gasparlamas32@gmail.com>
2024-09-12 16:08:58 -0300

    ta1 y ta2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const Card = ({ children }) => {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+};
+
+
+const Content = () => (
+  <div className="content">
+    <Card>
+      <h2>Card 1</h2>
+      <p>Esta es la card 1</p>
+      <p>Persona asignada: Hasbulla</p>
+      <p>Fecha de inicio: 11/9/2024</p>
+      <p>Fecha de fin: 11/10/2024</p>
+    </Card>
+
+    <Card>
+      <h2>Card 2</h2>
+      <p>Esta es la segunda card.</p>
+      <p>Persona asignada: Maradona</p>
+      <p>Fecha de inicio: 15/09/2024</p>
+      <p>Fecha de fin: 15/10/2024</p>
+    </Card>
+
+    <Card>
+      <h2>Card 3</h2>
+      <p>Esta es la card 3</p>
+      <p>Persona asignada: Wolverine</p>
+      <p>Fecha de inicio: 17/09/2024</p>
+      <p>Fecha de fin: 17/10/2024</p>
+    </Card>
+  </div>
+);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Content />
     </div>
   );
 }
+
 
 export default App;
